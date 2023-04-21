911434545a0522cd632e07a1cede3e2c5f15c2f5
Krish <krishmzn69@gmail.com>
2023-04-21 10:53:13 +0545

    results

--- src/pages/components/components/resume/kextractor.jsx
+++ src/pages/components/components/resume/kextractor.jsx
@@ -1,48 +1,46 @@
-import { useState } from 'react';
-import keyword_extractor from 'keyword-extractor';
+// import { useState } from 'react';
+// import keyword_extractor from 'keyword-extractor';
 
-export default function Keyw() {
-  const [jobTitle, setJobTitle] = useState('');
-  const [keywds, setKeywds] = useState([]);
+// export default function Keyw() {
+//   const [jobTitle, setJobTitle] = useState('');
+//   const [keywds, setKeywds] = useState([]);
   
-  const handleInputChange = (event) => {
-    setJobTitle(event.target.value);
-  };
+//   const handleInputChange = (event) => {
+//     setJobTitle(event.target.value);
+//   };
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    const extractedKeywds = keyword_extractor.extract(jobTitle, {
-      language: 'english',
-      remove_digits: true,
-      return_changed_case: true,
-      remove_duplicates: true,
-    });
-    setKeywds(extractedKeywds);
-    console.log(keywds)
-  };
+//   const handleFormSubmit = (event) => {
+//     event.preventDefault();
+//     const extractedKeywds = keyword_extractor.extract(jobTitle, {
+//       language: 'english',
+//       remove_digits: true,
+//       return_changed_case: true,
+//       remove_duplicates: true,
+//     });
+//     setKeywds(extractedKeywds);
+//     console.log(keywds)
+//   };
 
-  return (
-    <div>
-      <h1>Keyword Extractor</h1>
-      <form onSubmit={handleFormSubmit}>
-        <label>
-          Job Title:
-          <input type="text" value={jobTitle} onChange={handleInputChange} />
-        </label>
-        <button type="submit">Extract Keywds</button>
-      </form>
-      {keywds.length > 0 && (
-        <div>
-          <h2>Keywds:</h2>
-          <ul>
-            {keywds.map((keyword, index) => (
-              <li key={index}>{keyword}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
-  );
-}
-
-export{ keywds };
+//   return (
+//     <div>
+//       <h1>Keyword Extractor</h1>
+//       <form onSubmit={handleFormSubmit}>
+//         <label>
+//           Job Title:
+//           <input type="text" value={jobTitle} onChange={handleInputChange} />
+//         </label>
+//         <button type="submit">Extract Keywds</button>
+//       </form>
+//       {keywds.length > 0 && (
+//         <div>
+//           <h2>Keywds:</h2>
+//           <ul>
+//             {keywds.map((keyword, index) => (
+//               <li key={index}>{keyword}</li>
+//             ))}
+//           </ul>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
--- src/pages/resume.jsx
+++ src/pages/resume.jsx
@@ -3,17 +3,22 @@ import { useState } from "react";
 import { TagsInput } from "react-tag-input-component";
 
 // keywords extractor
-// import Keyw, { keywds } from './components/components/resume/kextractor';
 import keyword_extractor from 'keyword-extractor';
+
+// Lodaing
+import { Modal, Spinner } from 'daisyui';
 
 
 export default function Resume() {
   const [rdoc, setrdoc] = useState([]);
   const [email, setEmail] = useState("");
   const [response, setResponse] = useState(null);
-  const [jobTitle, setJobTitle] = useState('');
-  const [keywds, setKeywds] = useState([]);
+  const [jobTitle, setJobTitle] = useState(''); //texarea
+  const [keywds, setKeywds] = useState([]); //texarea
   const [keywords_received, setKeywords_received] = useState(keywds);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [tready, setReady] = useState(null)
+
   console.log(keywords_received, typeof (keywords_received))
   
   // keywords generator
@@ -23,15 +28,17 @@ export default function Resume() {
 
   const handleKeywds = (event) => {
     event.preventDefault();
+    setIsLoading(true);
     const extractedKeywds = keyword_extractor.extract(jobTitle, {
       language: 'english',
       remove_digits: true,
       return_changed_case: true,
       remove_duplicates: true,
-      return_max_ngrams: 3,
+      return_max_ngrams: 1,
     });
     // setKeywds(extractedKeywds);
     // console.log(keywds)
+    setIsLoading(false);
     setKeywords_received(extractedKeywds)
   };
 
@@ -49,7 +56,7 @@ export default function Resume() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setIsLoading(true);
     const formData = new FormData();
 
     // console.log('rdoc',rdoc)
@@ -70,32 +77,52 @@ export default function Resume() {
 
     const data = await res.json();
     setResponse(data);
+    setIsLoading(true);
   };
 
+
+  var rnames = [];
+  var mper = [];
+  var ccsv = "";
+
   const convertToHTML = (data) => {
-    let resumeNames = "";
-    let matchPercentage = "";
-    let csvFile = "";
+
     Object.keys(data).forEach((key) => {
       if (key == 'Resume Names') {
         // console.log(typeof(key))
         let val = data[key]
         // console.log(val)
         // console.log(val.slice(6))
-        resumeNames += `<strong>Resume Names: </strong>${val}<br>`;
+        // resumeNames += `<strong>Resume Names: </strong>${val}<br>`;
+        rnames.push(...val)
       }
       else if (key == 'Match Percentage') {
-        matchPercentage += `<strong>Match Percentage: </strong>${data[key]}<br>`;
+        // matchPercentage += `<strong>Match Percentage: </strong>${data[key]}<br>`;
+        let val = data[key]
+        mper.push(...val)
       }
       else if (key == 'csv_file') {
-        csvFile += `<strong>CSV File </strong>${data[key]}<br>`;
-      }
+        ccsv += data[key];
+        }
     });
-    return [resumeNames, matchPercentage, csvFile];
+    // setReady = 'ready'
+    return [rnames, mper, ccsv];
   };
+  
+  console.log('new arrays',rnames, mper, ccsv)
+  const resultPair = {
+    'names': rnames,
+    'match_percent': mper
+  }
 
+  // Object.entries(resultPair).forEach(entry => {
+  //   const [key, value] = entry;
+  //   console.log('keyvaluepairs',key, value)
+  //   var showResult = <tr> <td>{key}</td> <td>{value}</td> </tr>
+  // })
+  
   return (
-    <section className="flex justify-center items-center my-16 xl:px-52 lg:px-32 md:px-16 sm:px-10">
+    <section className="flex flex-col justify-center items-center my-16 xl:px-52 lg:px-32 md:px-16 sm:px-10 gap-5">
       <div className="card w-full bg-slate-50 shadow-2xl backdrop-filter backdrop-blur-lg bg-opacity-30 firefox:bg-opacity-90">
         <form onSubmit={handleSubmit} className="card-body">
           <div className="my-3">
@@ -142,8 +169,64 @@ export default function Resume() {
           <button type="submit" className="btn btn-primary  btn-active btn-xs sm:btn-sm md:btn-md lg:btn-lg" htmlFor="my-modal-6">Start Analysis</button>
 
         </form>
-        {response && <div dangerouslySetInnerHTML={{ __html: convertToHTML(response) }} />}
       </div>
+
+      <div className="card w-full bg-slate-50 shadow-2xl backdrop-filter backdrop-blur-lg bg-opacity-30 firefox:bg-opacity-90 p-5 overflow-x-auto">
+        <h1 className="text-4xl font-bold my-3">Result</h1>
+        {response && <div className="hidden" dangerouslySetInnerHTML={{ __html: convertToHTML(response) }} />}
+
+        {rnames.length > 0 && mper.length > 0 ? (
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Sno.</th>
+              <th>Resume Name</th>
+              <th>Match Percentage</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>&rarr;</td>
+            <td><strong>Download full csv report</strong></td>
+            <td><a href={ccsv} className="btn btn-primary">CSV File</a></td>
+          </tr>
+
+          {rnames.map((name, index) => (
+            <tr key={index} className="hover">
+              <td>{index+1}</td>
+              <td>{name}</td>
+              <td>{mper[index]}</td>
+            </tr>
+          ))}    
+          </tbody>
+        </table>
+      ) : (
+        <p>No results yet</p>
+      )}
+
+      </div>
+
+      {isLoading && (
+      <div className="modal">
+      <div className="modal-box">
+        <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+        <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+        <div className="modal-action">
+          <label htmlFor="my-modal" className="btn">Yay!</label>
+        </div>
+      </div>
+    </div>
+
+      // <div className="modal">
+      //   <div className="modal-box">
+      //     <h3 className="font-bold text-lg">Congratulations random Internet user!</h3>
+      //     <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+      //     <div className="modal-action">
+      //       <label htmlFor="my-modal" className="btn">Yay!</label>
+      //     </div>
+      //   </div>
+      // </div>
+    )}
     </section>
   );
 };
