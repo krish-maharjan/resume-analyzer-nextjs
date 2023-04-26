fff5b65667819c122a330a4b42fba869a996d5e8
Krish <krishmzn69@gmail.com>
2023-04-26 22:14:11 +0545

    carousel-started

--- src/pages/components/carousel.jsx
+++ src/pages/components/carousel.jsx
@@ -1,28 +1,49 @@
+import Image from 'next/image'
+
 export default function Carousel() {
+    const slides = {
+        1: [
+            {'img': '/image/4.png'},
+        ],
+        2: [
+            {'img': '/image/2.png'},
+        ],
+        3: [
+            {'img': '/image/3.png'},
+        ],
+        4: [
+            {'img': '/image/5.png'},
+        ],
+        5: [
+            {'img': '/image/4.png'},
+        ],
+        6: [
+            {'img': '/image/4.png'},
+        ],
+        7: [
+            {'img': '/image/4.png'},
+        ],
+        8: [
+            {'img': '/image/4.png'},
+        ],
+        
+    }
+
     return(
-        <section>
-            <div className="carousel carousel-center rounded-box">
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Pizza" />
-                </div> 
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Pizza" />
-                </div> 
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Pizza" />
-                </div> 
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Pizza" />
-                </div> 
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Pizza" />
-                </div> 
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Pizza" />
-                </div> 
-                <div className="carousel-item">
-                    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Pizza" />
+        <section className='relative'>
+            {Object.entries(slides).map(([key, values]) => (
+                <div className="absolute right-0">
+                    <a href={key - 4} className="btn btn-circle">❮</a> 
+                    <a href={key + 4} className="btn btn-circle">❯</a>
                 </div>
+            ))}
+
+            <div className="carousel carousel-center rounded-box">
+                {Object.entries(slides).map(([key, values]) => (
+                    <div className="carousel-item max-w-[25%]" id={key} >
+                        <img src={values[0].img} alt="Resume Analyzer" className="w-full h-auto object-cover" />
+                    </div> 
+                ))}
             </div>
         </section>
     )
