56ada3147b5151e9993cd9f36b318d862f0ad823
Krish <krishmzn69@gmail.com>
2023-06-25 12:52:21 +0545

    intro light

--- src/pages/components/intro.jsx
+++ src/pages/components/intro.jsx
@@ -8,7 +8,7 @@ export default function Intro() {
                     <div className="relative max-w-full">
                         <img src="./image/data.jpg" alt="image" className="min-w-[100vw] min-h-screen max-h-screen object-cover" />
                     </div>
-                    <div className="absolute min-w-[100vw] h-screen flex justify-center items-center flex-col bg-neutral-200/90">
+                    <div className="absolute min-w-[100vw] h-screen flex justify-center items-center flex-col bg-neutral-50/90">
                         <h1 className="text-5xl font-bold text-center">Resume Analyzer</h1>
                         <h2 className="pt-6 pb-10 text-2xl text-center">Automatically analyze resumes, save time and effort. Try Resume Analyzer right now</h2>
                         <p className="text-center">Click on the button below to <strong>start analyzing resumes</strong> or <strong>view source code</strong> </p><br />
