6e22ebfdff281ccedfd92dec326ac1841a6e7116
Krish <krishmzn69@gmail.com>
2023-09-29 11:35:09 +0545

    pricing hover

--- src/pages/components/stats.jsx
+++ src/pages/components/stats.jsx
@@ -9,7 +9,7 @@ export default function Stats() {
     return (
         <section className="flex justify-center w-full items-center p-0">
             <div className="hero min-h-fit min-w-full bg-[#e5e8f5]" style={{ backgroundImage: `url("./image/bg.png")` }}>
-                <ul className="flex flex-col lg:flex-row gap-12 lg:gap-32 py-12 justify-center items-center">
+                <ul className="flex flex-col lg:flex-row gap-12 lg:gap-32 py-12 justify-center items-center cursor-default">
                     {Object.entries(stats).map(([key, value]) => (
                         <li className="flex flex-col gap-2 justify-center items-center">
                             <div className="text-4xl text-neutral-900">{value}</div>
