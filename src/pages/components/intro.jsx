c2551cb5989cd932a9ce1b33f2577d78bd556710
Krish <krishmzn69@gmail.com>
2023-10-07 18:44:52 +0545

    intro font

--- src/pages/components/intro.jsx
+++ src/pages/components/intro.jsx
@@ -10,9 +10,9 @@ export default function Intro() {
                         <img src="./image/figma.png" alt="image" className="min-w-[100vw] min-h-screen max-h-screen object-cover" />
                     </div>
                     <main className="absolute min-w-[100vw] h-screen flex justify-center items-center flex-col px-6 gap-0">
-                        <h1 className="text-5xl font-bold text-center">Resume Analyzer</h1>
-                        <h2 className="pt-6 text-2xl text-center">Optimize Your Efficiency</h2>
-                        <h2 className="pb-8 text-2xl text-center">Streamline Resume and CV Analysis with Resume Analyzer</h2>
+                        <h1 className="text-6xl font-black text-center">Resume Analyzer</h1>
+                        <h2 className="pt-6 text-xl text-center">Optimize Your Efficiency</h2>
+                        <h2 className="pb-8 text-xl text-center">Streamline Resume and CV Analysis with Resume Analyzer</h2>
                         {/* <p className="text-center">Start for free!!!</p><br /> */}
                         <div className="form-control">
                             <div>
